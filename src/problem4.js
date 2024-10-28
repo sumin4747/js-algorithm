8c9ea9fa74082a7c228b5e32a59c0c33da794166
석수민 <rbbr2341@gmail.com>
2024-10-28 17:50:28 +0900

    4번 문제입니다.

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,18 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  let arr = Array.from(word);
+
+for (let i = 0; i < arr.length; i++) {
+  let ascii = arr[i].charCodeAt(0);  
+  if (ascii >= 65 && ascii <= 90) {  
+    arr[i] = String.fromCharCode(155 - ascii);
+  } else if (ascii >= 97 && ascii <= 122) {  
+    arr[i] = String.fromCharCode(219 - ascii);
+  }
+}
+
+let result = arr.join(''); 
+return result;
+
 }
 
 module.exports = problem4;
