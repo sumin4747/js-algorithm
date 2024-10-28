57ebd61a40b01dc1ca1471134a024694701de412
석수민 <rbbr2341@gmail.com>
2024-10-28 17:50:03 +0900

    3번 문제입니다.

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,22 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  
+  let cnt=0;
+  for(let i=1;i<=number;i++){
+    let arr=i.toString().split("");
+    for(let j=0;j<arr.length;j++){
+      if(arr[j]==3){
+        cnt++;
+      }
+      else if(arr[j]==6){
+        cnt++;
+      }
+      else if(arr[j]==9){
+        cnt++;
+      }
+    }
+  }
+
+  return cnt;
 }
 
 module.exports = problem3;
