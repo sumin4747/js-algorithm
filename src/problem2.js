071fef9abad0aee58ef7c31fd4bf6912d918578e
석수민 <rbbr2341@gmail.com>
2024-10-28 17:49:40 +0900

    2번 문제입니다.

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,29 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  
+  let arr = cryptogram.split("");
+  var cnt;
+  
+  while(true){
+
+    let cnt = true;
+    
+    for(let i=0;i<arr.length-1;i++){
+      if(arr[i]==arr[i+1])
+      {
+        arr.splice(i,2);
+        cnt = false;
+        break;
+      }
+      cnt = true;
+    }
+    n = arr.length;
+    if(cnt || n == 0)
+      break;
+  }
+  
+  let str = arr.join("")
+  return str;
+
 }
 
 module.exports = problem2;
