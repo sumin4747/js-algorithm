22e38f9d37077b7138f52c8e6b4f96ae4498d7f4
석수민 <rbbr2341@gmail.com>
2024-10-28 17:50:42 +0900

    5번 문제입니다.

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,17 @@
 function problem5(money) {
-  var answer;
-  return answer;
+    let arr = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+    let result = [];
+
+
+
+    for(let i of arr){
+        result.push(Math.floor(money/i));
+        money = money % i;
+
+    }
+
+    return result;
+
 }
 
 module.exports = problem5;
