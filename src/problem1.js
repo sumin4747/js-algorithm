b7f11bb14cd4eb99687ee83f39d85902683043c3
석수민 <rbbr2341@gmail.com>
2024-10-28 17:49:22 +0900

    1번 문제입니다.

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,53 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
-}
+  let Add = 0;
+  let Mul = 1;
+  let pobi_max;
+  let crong_max;
+  
+  if(pobi[0]!==pobi[1]-1 || crong[0]!==crong[1]-1)
+    return -1;
+
+  function name(arr){
+    for (let i=0; i<2; i++ ){
+      let Max = 0;
+      
+      let n=arr[i].toString();
+      
+      for(let j of n){
+        Add += Number(j);
+        Mul *= Number(j);
+      }
+      if(Add>Mul)
+        arr[i]=Add;
+      
+      else 
+        arr[i]=Mul;
+      
+      Add = 0;
+      Mul = 1;
+  }
+
+  if(arr[0]>arr[1])
+    Max = arr[0];
+  else 
+    Max = arr[1];
+
+  return Max;
+
+  }
+
+  pobi_max = name(pobi);
+  crong_max = name(crong);
+
+  if(pobi_max>crong_max)
+    return 1;
+  else if(crong_max>pobi_max)
+    return 2;
+  else if(crong_max==pobi_max) 
+    return 0;
+
+
+}  
+
 
 module.exports = problem1;
